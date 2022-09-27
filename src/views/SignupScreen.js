538c28ed34854439dbea6bbf43b558c9b15fd8a6
dray226 <helka084@uottawa.ca>
2022-09-27 17:18:16 -0400

    update layout signup screen

--- src/views/SignupScreen.js
+++ src/views/SignupScreen.js
@@ -38,12 +38,25 @@ function SignupScreen({ navigation }) {
 
 
 const styles = StyleSheet.create({
+    app: {
+        backgroundColor: 'white'
+      },
     input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
+        width: 350,
+        height: 55,
+        backgroundColor: '#42A5F5',
+        margin: 10,
+        padding: 8,
+        color: 'white',
+        borderRadius: 14,
+        fontSize: 18,
+        fontWeight: '500',
+      },
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
 });
 
 export default SignupScreen;
