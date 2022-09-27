4ad5ddb2405e271d96a52944c0f7021ae377fcd0
dray226 <helka084@uottawa.ca>
2022-09-27 18:07:04 -0400

    Update signIn screen

--- src/views/SigninScreen.js
+++ src/views/SigninScreen.js
@@ -1,19 +1,36 @@
 import * as React from 'react';
-import { Button, View, TextInput, StyleSheet} from 'react-native';
+import { Button, View, TextInput, StyleSheet,TouchableOpacity,Text,Image} from 'react-native';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 function SigninScreen({ navigation }) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        
+        
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:"#f08080" }}>
+        <Image source = {require("../../assets/favicon.png")}/>
+        
         <TextInput
         style={styles.input}
         value={String}
+        
         placeholder="username"
+        placeholderTextColor="#a9a9a9"
         />
         <TextInput
         style={styles.input}
         value={String}
+        
         placeholder="password"
+        placeholderTextColor="#a9a9a9"
         />
+
+    
+        
+        <Button title="Sign in" onPress={() => navigation.navigate('Home')} /> 
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        
         <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />      
       </View>
     );
@@ -22,11 +39,20 @@ function SigninScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
+        width:"80%",
+        backgroundColor:"#465881",
+        borderRadius:25,
+        height:50,
+        color: "white",
+        marginBottom:20,
+        justifyContent:"center",
+        padding:20
     },
+    forgot:{
+        color:"blue",
+        fontSize:11
+      }
+    
   });
 
 export default SigninScreen;
