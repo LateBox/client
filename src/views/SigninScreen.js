18c52d82530bd3f87652f184972294cd9cc99a1c
Jadbenn <43392239+Jadbenn@users.noreply.github.com>
2022-09-28 21:22:14 -0400

    Designed signIn and signup

--- src/views/SigninScreen.js
+++ src/views/SigninScreen.js
@@ -2,76 +2,131 @@ import * as React from 'react';
 import { Button, View, TextInput, StyleSheet,TouchableOpacity,Text,Image} from 'react-native';
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
+
 function SigninScreen({ navigation }) {
     return (
-        
-        
-      <View style={styles.container}>
-        <Image style = {styles.image} source = {require("../../assets/favicon.png")}/>
-        
-        <TextInput
-        style={styles.input}
-        value={String}
-        
-        placeholder="Username"
-        placeholderTextColor="#a9a9a9"
-        />
-        <TextInput
-        style={styles.input}
-        value={String}
-        secureTextEntry={true}
-        placeholder="Password"
-        placeholderTextColor="#a9a9a9"
-        /> 
+      <View style={styles.mainContainer}>
+        <View style={styles.topBar}>
 
-        <TouchableOpacity>
-          <Text style={styles.forgot}>Forgot Password?</Text>
-        </TouchableOpacity>
+        <Text style={styles.titleText} >Login</Text>
+          <Text style={styles.forgot}>
+            {"\n"}
+          </Text>
+          <TouchableOpacity>
+                <Text style={styles.newaccount} onPress={() => navigation.navigate('Register')}>Don't have an account? Sign up</Text>
+            </TouchableOpacity>
 
-        <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Home')}>
-        <Text >Sign in</Text>
-      </TouchableOpacity>
-        
-        
-        {/* <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />       */}
+        </View>
+
+
+          <View style={styles.body}>  
+          <Text style={styles.forgot}>
+            {"\n"}
+            {"\n"}
+            {"\n"}
+          </Text>
+          
+            <Text style={styles.sentence}>
+            {"PLEASE ENTER YOUR EMAIL                                     "}
+            </Text>  
+
+            <TextInput
+            style={styles.input}
+            value={String}
+            
+            placeholder="hello@latebox.com"
+            placeholderTextColor= 'rgba(77, 76, 76, 0.5)'
+            />
+
+            <Text style={styles.sentence}>
+            {"PLEASE ENTER YOUR PASSWORD                             "}
+            </Text>   
+
+            <TextInput
+            style={styles.input}
+            value={String}
+            secureTextEntry={true}
+            placeholder="Password"
+            placeholderTextColor='rgba(77, 76, 76, 0.5)'
+            /> 
+
+            <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Home')}>
+            <Text style={styles.loginBtnTxt} >login</Text>
+          </TouchableOpacity>
+            
+            
+            {/* <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />       */}
+          </View>
       </View>
     );
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f08080',
+    mainContainer: {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    topBar: {
+      alignItems: 'center',
+      backgroundColor: 'rgba(241, 136, 97, 0.8)',
+      height: 180,
+    },
+    body: {
+        flex: 3,
+        display: 'flex',
+        backgroundColor: 'white',
         alignItems: 'center',
-        justifyContent: 'center',
+        
+       
        },
        image :{
         marginBottom: 40
+  
      
       },
     input: {
+        marginTop: 7,
         width:"80%",
-        backgroundColor:"#465881",
+        backgroundColor:'rgba(245, 220, 189, 0.4)',
         borderRadius:25,
-        height:50,
+        height:30,
         color: "white",
         marginBottom:20,
         justifyContent:"center",
-        padding:20
+        padding:20,
+        fontSize: 12,
+      
     },
-    forgot:{
-        height: 30,
-  marginBottom: 30,
-      },
       loginBtn: {
-        width: "80%",
+        width: "65%",
         borderRadius: 25,
         height: 50,
         alignItems: "center",
         justifyContent: "center",
+        marginTop: 150,
+        backgroundColor: "rgba(33, 33, 98, 0.8)",
+      },
+      titleText: {
         marginTop: 40,
-        backgroundColor: "#ff1493",
+        fontSize: 28,
+        color: "#141445",
+      },
+      newaccount:{
+        height: 10,
+        marginBottom: 20,
+        fontSize: 14,
+        color: "#141445",
+        marginTop: 17,
+      },
+      sentence: {
+  
+        fontSize: 10,
+        color: "#8F8E8E",
+      },
+      loginBtnTxt: {
+        color: "white",
       },
     
   });
--- src/views/SignupScreen.js
+++ src/views/SignupScreen.js
@@ -3,90 +3,159 @@ import { Button, View, TextInput, StyleSheet,TouchableOpacity,Text,Image } from 
 
 function SignupScreen({ navigation }) {
     return (
-        <View style={styles.container}>
-            <Image style={styles.image} source={require("../../assets/favicon.png")} />
+        
+        <View style={styles.mainContainer}>
 
-            <TextInput
-                style={styles.input}
-                value={String}
-                placeholder="Full name"
-                placeholderTextColor="#a9a9a9"
-                autoCapitalize={"none"}
-            />
+            <View style={styles.topBar}>
 
-            <TextInput
-                style={styles.input}
-                value={String}
-                placeholder="E=mail Address"
-                placeholderTextColor="#a9a9a9"
+                <Text style={styles.titleText} >Create new Account</Text>
+                <Text style={styles.forgot}>
+                    {"\n"}
+                </Text>
                 
-            />
+                <TouchableOpacity>
+                    <Text style={styles.existing} onPress={() => navigation.navigate('Login')}>Already Registered? Login</Text>
+                </TouchableOpacity>
 
-            <TextInput
-                style={styles.input}
-                keyboardType={'phone-pad'}
-                value={String}
-                placeholder="Phone Number"
-                maxLength={10}
-                placeholderTextColor="#a9a9a9"
-            />
+            </View>
 
-            <TextInput
-                style={styles.input}
-                value={String}
-                secureTextEntry={true}
-                placeholder="Password"
-                placeholderTextColor="#a9a9a9"
-            />
+            <View style={styles.body}>
+                <Text style={styles.forgot}>
+                    {"\n"}
+                 {"\n"}
+                 {"\n"}
+                </Text>
 
-            <TouchableOpacity style={styles.signUpBtn} onPress={() => navigation.navigate('Home')}>
-                <Text>Sign up</Text>
-            </TouchableOpacity>
+                <Text style={styles.sentence}>
+                    {"PLEASE ENTER YOUR NAME                                     "}
+                </Text> 
 
-            {/* <Button title="Sign up" onPress={() => navigation.navigate('Login')} />
+                <TextInput
+                    style={styles.input}
+                    value={String}
+                    placeholder="Full name"
+                    placeholderTextColor="#a9a9a9"
+                    autoCapitalize={"none"}
+                />
 
-            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} /> */}
+                <Text style={styles.sentence}>
+                    {"PLEASE ENTER YOUR EMAIL                                     "}
+                </Text> 
+
+                <TextInput
+                    style={styles.input}
+                    value={String}
+                    placeholder="E=mail Address"
+                    placeholderTextColor="#a9a9a9"
+                    
+                />
+
+                <Text style={styles.sentence}>
+                    {"PLEASE ENTER YOUR PASSWORD                              "}
+                </Text> 
+
+                <TextInput
+                    style={styles.input}
+                    value={String}
+                    secureTextEntry={true}
+                    placeholder="Password"
+                    placeholderTextColor="#a9a9a9"
+                />
+
+                <Text style={styles.sentence}>
+                    {"PLEASE ENTER YOUR PHONE NUMBER                      "}
+                </Text> 
+
+                <TextInput
+                    style={styles.input}
+                    keyboardType={'phone-pad'}
+                    value={String}
+                    placeholder="Phone Number"
+                    maxLength={10}
+                    placeholderTextColor="#a9a9a9"
+                />
+
+                
+
+                <TouchableOpacity style={styles.signUpBtn} onPress={() => navigation.navigate('Home')}>
+                    <Text style={styles.signUpBtnTxt}>Sign up</Text>
+                </TouchableOpacity>
+
+                {/* <Button title="Sign up" onPress={() => navigation.navigate('Login')} />
+
+                <Button title="Go to Home" onPress={() => navigation.navigate('Home')} /> */}
 
 
+            </View>
         </View>
     );
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f08080',
+    mainContainer: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+    topBar: {
         alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: 'rgba(241, 136, 97, 0.8)',
+        height: 180,
+      },
+    body: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        
     },
     image: {
         marginBottom: 40
 
     },
     input: {
-        width: "80%",
-        backgroundColor: "#465881",
-        borderRadius: 25,
-        height: 50,
+        marginTop: 7,
+        width:"80%",
+        backgroundColor:'rgba(245, 220, 189, 0.4)',
+        borderRadius:25,
+        height:30,
         color: "white",
-        marginBottom: 20,
-        justifyContent: "center",
-        padding: 20
+        marginBottom:20,
+        justifyContent:"center",
+        padding:20,
+        fontSize: 12,
     },
-    forgot: {
-        height: 30,
-        marginBottom: 30,
-    },
+ 
     signUpBtn: {
-        width: "80%",
+        width: "65%",
         borderRadius: 25,
         height: 50,
         alignItems: "center",
         justifyContent: "center",
-        marginTop: 40,
-        backgroundColor: "#ff1493",
+        marginTop: 70,
+        backgroundColor: "rgba(33, 33, 98, 0.8)",
     },
+    signUpBtnTxt:{
+        color: "white",
+
+    },
+    titleText: {
+        marginTop: 40,
+        fontSize: 28,
+        color: "#141445",
+    },
+    existing:{
+        height: 10,
+        marginBottom: 20,
+        fontSize: 14,
+        color: "#141445",
+        marginTop: 17,
+    },
+    sentence: {
+  
+        fontSize: 10,
+        color: "#8F8E8E",
+      },
 
 
 
