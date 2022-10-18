c67cf6777bea0b33e31a5ce28f62df25cbc9667f
uygaraktuna <43392169+uygaraktuna@users.noreply.github.com>
2022-10-18 00:54:37 -0400

    Added Coordinate class for the map

---
+++ src/models/coordinate.js
@@ -0,0 +1,10 @@
+class Coordinate{
+    constructor(id, ownerId, lat, long){
+        this.id = id;
+        this.ownerId = ownerId;
+        this.lat = lat;
+        this.long = long;
+    }
+}
+
+export default Coordinate;
