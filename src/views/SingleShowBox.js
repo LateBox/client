ecc332f77e0f16fb362be7f7779c4b6f0bc738af
Gokhan Duzel <43392060+GokhanDuzel@users.noreply.github.com>
2022-12-04 15:12:07 -0500

    Single Show Box UI

--- src/views/SingleShowBox.js
+++ src/views/SingleShowBox.js
@@ -42,6 +42,7 @@ function SingleShowBox({ navigation, props }) {
   const Decrement = () => {
     if (orderQuantity > 1) {
       setOrderQuantity(orderQuantity - 1);
+      setPrice(price - initialPrice);
     }
   };
 
@@ -115,39 +116,23 @@ function SingleShowBox({ navigation, props }) {
             <Text>Restaurant Name</Text>
           </View>
         </View>
+
         <TouchableOpacity
           style={{
             width: 50,
             paddingRight: SIZES.padding * 2,
             justifyContent: "center",
           }}
-        >
-          {/* <Image
-            source={icons.star}
-            resizeMode="contain"
-            style={{
-              width: 30,
-              height: 30,
-            }}
-          /> */}
-        </TouchableOpacity>
+        ></TouchableOpacity>
       </View>
     );
   };
 
+  // OK
+
   function renderFoodInfo() {
     return (
-      <View
-        horizontal
-        pagingEnabled
-        scrollEventThrottle={16}
-        snapToAlignment="center"
-        showsHorizontalScrollIndicator={false}
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          { useNativeDriver: false }
-        )}
-      >
+      <View>
         <View key={`menu-${itemId}`} style={{ alignItems: "center" }}>
           <View style={{ height: SIZES.height * 0.35 }}>
             {/* Food Image */}
@@ -163,6 +148,8 @@ function SingleShowBox({ navigation, props }) {
             {/* Quantity */}
             <View
               style={{
+                position: "absolute",
+                bottom: -20,
                 width: SIZES.width,
                 height: 50,
                 justifyContent: "center",
@@ -208,54 +195,54 @@ function SingleShowBox({ navigation, props }) {
                 <Text>+</Text>
               </TouchableOpacity>
             </View>
-            {/* Name & Description */}
-            <View
+          </View>
+
+          {/* Name & Description */}
+          <View
+            style={{
+              width: SIZES.width,
+              alignItems: "center",
+              marginTop: 15,
+              paddingHorizontal: SIZES.padding * 2,
+            }}
+          >
+            <Text
               style={{
-                bottom: -20,
-                width: SIZES.width,
-                alignItems: "center",
-                marginTop: 15,
-                paddingHorizontal: SIZES.padding * 2,
+                marginVertical: 10,
+                textAlign: "center",
+                padding: 20,
               }}
             >
-              <Text
-                style={{
-                  padding: 30,
-                }}
-              >
-                some food description
+              itemName - ${price}
+            </Text>
+            <View style={{ backgroundColor: COLORS.white }}>
+              <Text>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
               </Text>
             </View>
-
-            {/* Price */}
-            <View
-              style={{
-                justifyContent: "center",
-                flexDirection: "row",
-                alignItems: "center",
-                width: 50,
-                backgroundColor: COLORS.white,
-                borderRadius: 25,
-              }}
-            >
-              <Text style={{ textAlign: "center" }}>{price} $</Text>
-            </View>
-
-            {/* Add to Cart */}
-            <TouchableOpacity
-              style={{
-                height: 50,
-                alignItems: "center",
-                justifyContent: "center",
-                borderRadius: SIZES.radius,
-                paddingHorizontal: SIZES.padding * 3,
-                backgroundColor: COLORS.lightGray3,
-              }}
-              onPress={''}
-            >
-              <Text>Add to Cart</Text>
-            </TouchableOpacity>
           </View>
+          {/* Price
+          <View
+            style={{
+              justifyContent: "center",
+              flexDirection: "row",
+              alignItems: "center",
+              width: 50,
+              backgroundColor: COLORS.white,
+              borderRadius: 25,
+            }}
+          >
+            <Text style={{ textAlign: "center" }}>{price} $</Text>
+          </View> */}
+          <TouchableOpacity style={styles.toCartButton} onPress={""}>
+            <Text>Add to Cart</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -277,14 +264,13 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.lightGray2,
   },
   toCartButton: {
-    marginLeft: 95,
-    width: 200,
-    backgroundColor: COLORS.white,
-    borderColor: COLORS.black,
-    alignItems: "center",
-    justifyContent: "center",
-    borderWidth: 2,
+    width: "65%",
     borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    marginTop: 100,
+    justifyContent: "center",
+    backgroundColor: "rgb(80, 200, 120)",
   },
 });
 
